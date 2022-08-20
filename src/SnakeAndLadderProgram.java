e661b00a881be83375e087dd78e3e0eba736bfa4
vasu <vasanthakumarnr6@gmail.com>
2022-08-20 16:58:21 -0700

    added - Starting position of the player

---
+++ src/SnakeAndLadderProgram.java
@@ -0,0 +1,15 @@
+public class SnakeAndLadderProgram {
+        int startingPosition=0;
+
+        public void showPosition(){
+            System.out.println("Welcome to SNAKE AND LADDER PROGRAM");
+            System.out.println("Starting position of the player = " + startingPosition);
+
+        }
+
+        public static void main(String[] args) {
+            SnakeAndLadderProgram P1 = new SnakeAndLadderProgram();
+
+            P1.showPosition();
+        }
+    }
