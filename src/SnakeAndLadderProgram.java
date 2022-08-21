329d04f8cabf2acbd784b9457eabfa870d74beec
vasu <vasanthakumarnr6@gmail.com>
2022-08-20 18:54:12 -0700

    added - Input by user from 1-6

--- src/SnakeAndLadderProgram.java
+++ src/SnakeAndLadderProgram.java
@@ -7,9 +7,16 @@ public class SnakeAndLadderProgram {
 
         }
 
+    public int rollingDice()
+    {
+        int diceNumber = (int) Math.floor(Math.random()*10)%6+1;
+        return diceNumber;
+    }
+
         public static void main(String[] args) {
             SnakeAndLadderProgram P1 = new SnakeAndLadderProgram();
 
             P1.showPosition();
+            System.out.println("Input from user by rolling dice : " + P1.rollingDice());
         }
     }
