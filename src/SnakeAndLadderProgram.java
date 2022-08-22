df598a7598fc9cd9690385cb10a3d0cbd1324121
vasu <vasanthakumarnr6@gmail.com>
2022-08-22 05:53:15 -0700

    added - Report the number of

--- src/SnakeAndLadderProgram.java
+++ src/SnakeAndLadderProgram.java
@@ -1,69 +1,30 @@
+import java.util.Random;
+
 public class SnakeAndLadderProgram {
-        int startingPosition=0;
+
+
+        int startingPosition = 0;
         public static final int isNoPlay = 0;
         public static final int isSnake = 1;
         public static final int isLadder = 2;
 
-        int playerInitPosition=0;
-        int newPosition=0;
+        int playerInitPosition = 0;
+        int newPosition = 0;
+        int rollingCount = 0;
         int optionNumber;
         int diceNumber;
 
-        public void showPosition(){
-            System.out.println("Welcome to SNAKE AND LADDER PROGRAM");
-            System.out.println("Starting position of the player = " + startingPosition);
-            System.out.println("Starting position of the player = " + playerInitPosition);
+        Random randomNumber = new Random();
+    public int showPosition() {
 
+            int diceNumber = randomNumber.nextInt(6) + 1;
+            rollingCount++;
+            System.out.println("Dice Count : " + rollingCount);
+            return diceNumber;
         }
 
-    public int rollingDice()
-    {
-        int diceNumber = (int) Math.floor(Math.random()*10)%6+1;
-        return diceNumber;
-    }
-    public void optionPlay(int diceNumber)
-    {
-        int optionNumber= (int) Math.floor(Math.random()*10)%3+1;
-        System.out.println("Option Number : " + optionNumber);
-
-        switch (optionNumber)
-        {
-            case isNoPlay:
-                System.out.println("You Got No Play");
-                newPosition = 0;
-                break;
-
-            case isLadder:
-                System.out.println("You Got Ladder");
-                playerInitPosition = playerInitPosition + diceNumber;
-                if (playerInitPosition == 100) {
-                    System.out.println("There is no such position");
-                    playerInitPosition = --diceNumber;
-                }
-                break;
-
-            case isSnake:
-                System.out.println("You got snake");
-                playerInitPosition = playerInitPosition - diceNumber;
-                if(playerInitPosition < 0)
-                {
-                    playerInitPosition = 0;
-                }
-                break;
-
+        public static void main(String args[]) {
+            Random randomNumber = new Random();
         }
-    }
-        public static void main(String[] args) {
-            SnakeAndLadderProgram P1 = new SnakeAndLadderProgram();
 
-            P1.showPosition();
-            System.out.println("Input from user by rolling dice : " + P1.rollingDice());
-            P1.optionPlay(P1.rollingDice());
-            P1.showPosition();
-            P1.optionPlay(P1.rollingDice());
-            P1.showPosition();P1.optionPlay(P1.rollingDice());
-            P1.showPosition();
-            P1.optionPlay(P1.rollingDice());
-            P1.showPosition();
-        }
     }
